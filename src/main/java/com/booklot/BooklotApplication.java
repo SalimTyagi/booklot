7287787ee5e807c00cbb7acf4c079d2f4e8a39ab
NISAR Brick Field <nisar@NISARs-MacBook-Air.local>
2025-06-05 10:53:41 +0530

    Initial commit

---
+++ src/main/java/com/booklot/BooklotApplication.java
@@ -0,0 +1,13 @@
+package com.booklot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooklotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booklot/BooklotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booklot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
